6e581b049f5211303b8b99c90fbdf12c2b71a98a
Orlando Gonzalez <ogonzalezs@gmail.com>
2023-01-23 23:37:45 -0300

    Creando Transacciones & Actualizacion Script DB

---
+++ ManejoPresupuesto/Controllers/TransaccionesController.cs
@@ -0,0 +1,36 @@
+﻿using ManejoPresupuesto.Interfaces;
+using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TransaccionesController : Controller
+    {
+        private readonly IServicioUsuario servicioUsuario;
+        private readonly IRepositorioCuentas repositorioCuentas;
+
+        public TransaccionesController(IServicioUsuario servicioUsuario
+                            , IRepositorioCuentas repositorioCuentas)
+        {
+            this.servicioUsuario = servicioUsuario;
+            this.repositorioCuentas = repositorioCuentas;
+        }
+
+
+        public async Task<IActionResult> Crear() 
+        {
+            var usuarioId = servicioUsuario.ObtenerUsuarioId();
+            var modelo = new TransaccionCreacionVM();
+            modelo.Cuentas = await ObtenerCuentas(usuarioId);
+
+            return View(modelo);
+        }
+
+        private async Task<IEnumerable<SelectListItem>> ObtenerCuentas (int usuarioId) 
+        {
+            var cuentas = await repositorioCuentas.Buscar(usuarioId);
+            return cuentas.Select(x => new SelectListItem(x.Nombre, x.CuentaId.ToString()));
+        }
+    }
+}
---
+++ ManejoPresupuesto/Models/TransaccionCreacionVM.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TransaccionCreacionVM : Transaccion
+    {
+        public IEnumerable<SelectListItem> Cuentas { get; set; }
+
+        public IEnumerable<SelectListItem> Categorias { get; set; }
+    }
+}
